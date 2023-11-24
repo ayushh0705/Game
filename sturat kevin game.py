aa7ca64844414a8980a8dc3a22165ac607ba79cb
ayushh0705 <150360676+ayushh0705@users.noreply.github.com>
2023-11-24 16:07:24 +0530

    Update sturat kevin game.py

--- sturat kevin game.py
+++ sturat kevin game.py
@@ -1,25 +1,35 @@
 def minion_game(string):
-    vowel = "AEIOU"
+    # Define the vowels
+    vowels = "AEIOU"
+    
+    # Get the length of the input string
     length = len(string)
-    kevin = 0
-    stuart = 0
+    
+    # Initialize scores for Kevin and Stuart
+    kevin_score = 0
+    stuart_score = 0
 
+    # Iterate through each character in the string
     for i in range(length):
-        if string[i] in vowel:
-            kevin += length - i
+        # Check if the character is a vowel
+        if string[i] in vowels:
+            # If it's a vowel, increment Kevin's score by the number of substrings
+            kevin_score += length - i
         else:
-            stuart += length - i
+            # If it's a consonant, increment Stuart's score by the number of substrings
+            stuart_score += length - i
 
-    if kevin > stuart:
-        print(f"Kevin {kevin}")
-    elif stuart > kevin:
-        print(f"Stuart {stuart}")
+    # Compare scores and print the result
+    if kevin_score > stuart_score:
+        print(f"Kevin {kevin_score}")
+    elif stuart_score > kevin_score:
+        print(f"Stuart {stuart_score}")
     else:
         print("Draw")
 
-
-
-
-if __name__ == '__main__':
-    s = input()
+if __name__ == "__main__":
+    # Get user input for the string
+    s = input("Enter the string: ").upper()
+    
+    # Call the minion_game function with the input string
     minion_game(s)
